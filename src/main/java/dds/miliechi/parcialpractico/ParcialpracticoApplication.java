b096f17b755b30df2e233c3d3aed10021feb8a4f
Ezequiel Saidman <esaidman@frba.utn.edu.ar>
2022-07-11 16:50:26 -0300

    Commit inicial

---
+++ src/main/java/dds/miliechi/parcialpractico/ParcialpracticoApplication.java
@@ -0,0 +1,13 @@
+package dds.miliechi.parcialpractico;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ParcialpracticoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ParcialpracticoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/dds/miliechi/parcialpractico/ParcialpracticoApplicationTests.java
@@ -0,0 +1,13 @@
+package dds.miliechi.parcialpractico;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ParcialpracticoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
