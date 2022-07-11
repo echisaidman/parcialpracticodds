58bf3cdc20fe83e5725c826b4c0e8e234447a71f
Ezequiel Saidman <esaidman@frba.utn.edu.ar>
2022-07-11 17:44:48 -0300

    Renombro archivo Main

--- src/main/java/dds/miliechi/parcialpractico/Main.java
+++ src/main/java/dds/miliechi/parcialpractico/Main.java
@@ -4,7 +4,7 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class ParcialpracticoApplication {
+public class Main {
 
 	public static void main(String[] args) {
 		SpringApplication.run(ParcialpracticoApplication.class, args);
